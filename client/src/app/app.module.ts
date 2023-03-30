1bb60710a832c936039a160365c98a783ee178db
Reza Norouz Zadeh <reza.nrzdh@gmail.com>
2023-03-30 15:08:44 +0330

    update and add

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {MainLayoutComponent} from "./shared/layout/main-layout/main-layout.component";
+import {AdminLayoutComponent} from "./shared/layout/admin-layout/admin-layout.component";
+
 import { AppComponent } from './app.component';
 import {RoutingModule} from "./routing.module";
 import {SharedModule} from "./shared/shared.module";
@@ -8,7 +11,9 @@ import {CoreModule} from "./core/core.module";
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        MainLayoutComponent,
+        AdminLayoutComponent
     ],
     imports: [
         BrowserModule,
--- client/src/app/routing.module.ts
+++ client/src/app/routing.module.ts
@@ -1,13 +1,22 @@
 import {NgModule} from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import {MainLayoutComponent} from "./shared/layout/main-layout/main-layout.component";
 
 const routes: Routes = [
-    { path: "", loadChildren: () => import("./feature/home/home.module").then(m => m.HomeModule) },
-    { path: "portfolio", loadChildren: () => import("./feature/portfolio/portfolio.module").then(m => m.PortfolioModule) },
-    { path: "blog", loadChildren: () => import("./feature/blog/blog.module").then(m => m.BlogModule) },
-    { path: "order", loadChildren: () => import("./feature/order/order.module").then(m => m.OrderModule) },
-    { path: "about", loadChildren: () => import("./feature/about/about.module").then(m => m.AboutModule) },
-    { path: "contact", loadChildren: () => import("./feature/contact/contact.module").then(m => m.ContactModule) },
+    {
+        path: "",
+        component: MainLayoutComponent,
+        children: [
+            { path: "", loadChildren: () => import("./feature/home/home.module").then(m => m.HomeModule) },
+            { path: "portfolio", loadChildren: () => import("./feature/portfolio/portfolio.module").then(m => m.PortfolioModule) },
+            { path: "blog", loadChildren: () => import("./feature/blog/blog.module").then(m => m.BlogModule) },
+            { path: "order", loadChildren: () => import("./feature/order/order.module").then(m => m.OrderModule) },
+            { path: "about", loadChildren: () => import("./feature/about/about.module").then(m => m.AboutModule) },
+            { path: "contact", loadChildren: () => import("./feature/contact/contact.module").then(m => m.ContactModule) },
+        ]
+    },
+    { path: "login", loadChildren: () => import("./feature/auth/login/login.module").then(m => m.LoginModule) },
+    { path: "register", loadChildren: () => import("./feature/auth/register/register.module").then(m => m.RegisterModule) }
 ];
 
 @NgModule({
