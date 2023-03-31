ce8be3c6e90fce0c0c0ca42be839fa129cb339af
Reza Norouz Zadeh <reza.nrzdh@gmail.com>
2023-03-31 12:34:52 +0330

    update

---
+++ client/src/app/shared/component/textbox/textbox.component.ts
@@ -0,0 +1,40 @@
+import { Component, Input } from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
+
+@Component({
+    selector: 'app-textbox',
+    templateUrl: './textbox.component.html',
+    styleUrls: ['./textbox.component.scss'],
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: TextboxComponent,
+            multi: true
+        }
+    ]
+})
+export class TextboxComponent implements ControlValueAccessor {
+
+    @Input() title: string;
+    @Input() type: "text" | "password" | "submit" | "email" | "tel";
+    @Input() name: string;
+    @Input() formGroup: any = "";
+
+    value: string = "";
+    onChange: (value: any) => void;
+    onTouched: () => void;
+
+    constructor() { }
+
+    writeValue(obj: any) {
+        this.value = obj;
+    }
+
+    registerOnChange(fn: any) {
+        this.onChange = fn;
+    }
+
+    registerOnTouched(fn: any) {
+        this.onTouched = fn;
+    }
+}
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -2,18 +2,25 @@ import {NgModule} from "@angular/core";
 import {HeaderComponent} from "./component/header/header.component";
 import {RouterModule} from "@angular/router";
 import {ButtonComponent} from "./component/button/button.component";
+import {TextboxComponent} from "./component/textbox/textbox.component";
+import {CommonModule} from "@angular/common";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
     declarations: [
         HeaderComponent,
-        ButtonComponent
+        ButtonComponent,
+        TextboxComponent
     ],
     imports: [
+        CommonModule,
+        FormsModule,
         RouterModule
     ],
     exports: [
         HeaderComponent,
-        ButtonComponent
+        ButtonComponent,
+        TextboxComponent
     ]
 })
 export class SharedModule {}
