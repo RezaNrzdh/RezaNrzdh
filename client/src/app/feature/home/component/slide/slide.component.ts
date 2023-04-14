ee72f29961f681281350b5e7e912815957fdb809
Reza Norouz Zadeh <reza.nrzdh@gmail.com>
2023-04-14 18:32:20 +0430

    update

---
+++ client/src/app/feature/home/component/slide/slide.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-slide',
+  templateUrl: './slide.component.html',
+  styleUrls: ['./slide.component.scss']
+})
+export class SlideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- client/src/app/feature/home/home.module.ts
+++ client/src/app/feature/home/home.module.ts
@@ -3,10 +3,18 @@ import {HomeRoutingModule} from "./home-routing.module";
 import {SharedModule} from "../../shared/shared.module";
 import {HomeComponent} from "./home.component";
 import {CommonModule} from "@angular/common";
+import {BlogComponent} from "./component/blog/blog.component";
+import {ExperienceComponent} from "./component/experience/experience.component";
+import {PortfolioComponent} from "./component/portfolio/portfolio.component";
+import {SlideComponent} from "./component/slide/slide.component";
 
 @NgModule({
     declarations: [
-        HomeComponent
+        HomeComponent,
+        BlogComponent,
+        ExperienceComponent,
+        PortfolioComponent,
+        SlideComponent
     ],
     providers: [],
     imports: [
