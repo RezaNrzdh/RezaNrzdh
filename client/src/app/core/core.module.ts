addb81f325cc7e04bd8d4701dfa0d69628b99878
Reza Norouz Zadeh <reza.nrzdh@gmail.com>
2023-05-29 17:43:33 +0330

    add new Services to Core module

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,7 +1,12 @@
 import {NgModule} from "@angular/core";
+import {PortfolioService} from "./services/portfolio.service";
+import {SkillsService} from "./services/skills.service";
 
 @NgModule({
-    providers:[],
+    providers:[
+        PortfolioService,
+        SkillsService
+    ],
     imports:[]
 })
 export class CoreModule {}
