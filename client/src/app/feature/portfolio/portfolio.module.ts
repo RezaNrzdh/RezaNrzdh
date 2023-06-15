934e770b474545c2b035f11674b0462e9aa2b632
Reza Norouz Zadeh <reza.nrzdh@gmail.com>
2023-06-15 18:34:38 +0330

    update

--- client/src/app/feature/portfolio/portfolio.module.ts
+++ client/src/app/feature/portfolio/portfolio.module.ts
@@ -3,10 +3,12 @@ import {SharedModule} from "../../shared/shared.module";
 import {PortfolioComponent} from "./detail/portfolio.component";
 import {PortfolioRoutingModule} from "./portfolio-routing.module";
 import {PortfolioListComponent} from "./list/portfolio-list.component";
+import {SliderComponent} from "./detail/component/slider/slider.component";
 
 @NgModule({
     declarations: [
         PortfolioComponent,
+        SliderComponent,
         PortfolioListComponent
     ],
     providers: [],
