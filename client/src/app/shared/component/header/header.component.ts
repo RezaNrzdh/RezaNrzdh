32b41d413bcf2e16ef019e640cf630fd8da6b78c
Reza Norouz Zadeh <reza.nrzdh@gmail.com>
2023-06-26 22:51:38 +0330

    update

--- client/src/app/shared/component/header/header.component.ts
+++ client/src/app/shared/component/header/header.component.ts
@@ -1,8 +1,35 @@
-import {Component} from "@angular/core";
+import {Component, HostBinding, OnInit} from "@angular/core";
+import {Router, NavigationStart} from "@angular/router";
 
 @Component({
     selector: "app-header",
     templateUrl: "header.component.html",
     styleUrls: ["header.component.scss"]
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+    constructor( private router: Router) {
+    }
+
+    @HostBinding("attr.indexpage") CurrentRoute = true;
+
+    ngOnInit() {
+        this.router.events.subscribe(event => {
+            if (event instanceof NavigationStart) {
+                if(event.url === "/"){
+                    this.CurrentRoute = false;
+                }
+                else{
+                    this.CurrentRoute = true;
+                }
+            }
+        })
+
+        if(this.router.url === "/"){
+            this.CurrentRoute = false;
+        }
+        else{
+            this.CurrentRoute = true;
+        }
+    }
+
+}
