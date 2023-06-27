babde1a085ce93e553d5e5acdaeb6ac27e521543
Reza Norouz Zadeh <reza.nrzdh@gmail.com>
2023-06-27 13:00:03 +0330

    update

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,26 @@
-import {Component, enableProdMode} from '@angular/core';
+import {Component, enableProdMode, HostBinding} from '@angular/core';
+import {Router,NavigationStart} from "@angular/router";
 enableProdMode();
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'RezaNrzdh';
+    title = 'RezaNrzdh';
+
+    @HostBinding("class.indexpage") IndexPage = false;
+
+    constructor(private router: Router) {
+        this.router.url === "/" ? this.IndexPage = true : this.IndexPage = false;
+
+        this.router.events.subscribe(event => {
+            if(event instanceof NavigationStart){
+                event.url === "/" ? this.IndexPage = true : this.IndexPage = false;
+            }
+        })
+    }
+
+
 }
--- client/src/app/shared/component/header/header.component.ts
+++ client/src/app/shared/component/header/header.component.ts
@@ -1,35 +1,9 @@
-import {Component, HostBinding, OnInit} from "@angular/core";
-import {Router, NavigationStart} from "@angular/router";
+import {Component} from "@angular/core";
 
 @Component({
     selector: "app-header",
     templateUrl: "header.component.html",
     styleUrls: ["header.component.scss"]
 })
-export class HeaderComponent implements OnInit {
-    constructor( private router: Router) {
-    }
-
-    @HostBinding("attr.indexpage") CurrentRoute = true;
-
-    ngOnInit() {
-        this.router.events.subscribe(event => {
-            if (event instanceof NavigationStart) {
-                if(event.url === "/"){
-                    this.CurrentRoute = false;
-                }
-                else{
-                    this.CurrentRoute = true;
-                }
-            }
-        })
-
-        if(this.router.url === "/"){
-            this.CurrentRoute = false;
-        }
-        else{
-            this.CurrentRoute = true;
-        }
-    }
-
+export class HeaderComponent {
 }
