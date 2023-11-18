64ced839d07ab3951bacb4196614e47754061856
RezaNrzdh <reza.nrzdh@gmail.com>
2023-11-18 11:53:55 +0330

    edit contact page

--- client/src/app/feature/contact/contact.component.ts
+++ client/src/app/feature/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
     selector: 'app-contact',
@@ -9,6 +10,19 @@ export class ContactComponent implements OnInit {
 
     constructor() { }
 
+    contactForm: FormGroup;
+
     ngOnInit(): void {
+        this.contactForm = new FormGroup({
+            "name": new FormControl("null"),
+            "email": new FormControl("null"),
+            "phone": new FormControl("null"),
+            "subject": new FormControl("null"),
+            "comment": new FormControl("null"),
+        })
+    }
+
+    GetContactFormData() {
+        console.log(this.contactForm.value);
     }
 }
