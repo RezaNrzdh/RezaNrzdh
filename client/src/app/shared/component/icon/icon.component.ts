2372ab0c5536c08db0eddc6c3b91ca49054811c0
RezaNrzdh <reza.nrzdh@gmail.com>
2023-11-19 14:15:45 +0330

    improve icon

--- client/src/app/shared/component/icon/icon.component.ts
+++ client/src/app/shared/component/icon/icon.component.ts
@@ -13,7 +13,7 @@ export class IconComponent implements OnInit {
 
     @Input() icon: string;
     @Input() size: "16" | "20" | "24" | "32" | "48" = "24";
-    @Input() color: "primary" | "secondary" | "success" | "danger" | "info" | "gray1" | "gray2" | "gray3" | "white" | "textTint";
+    @Input() color: "primary" | "secondary" | "gray1" | "gray2" | "gray3" | "gray4" | "gray5" | "white" | "dark";
 
     ngOnInit() {
         this.matIconRegistry.addSvgIcon(
