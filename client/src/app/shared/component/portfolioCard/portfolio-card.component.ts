ce6d0162e02c1ebf990a11ca1d95b7b7d51a44c7
RezaNrzdh <reza.nrzdh@gmail.com>
2023-11-20 16:20:15 +0330

    update portfolio card

--- client/src/app/shared/component/portfolioCard/portfolio-card.component.ts
+++ client/src/app/shared/component/portfolioCard/portfolio-card.component.ts
@@ -6,10 +6,15 @@ import {Component, Input, OnInit} from "@angular/core";
     styleUrls: ["portfolio-card.component.scss"]
 })
 export class PortfolioCardComponent implements OnInit {
+
+    @Input() isOutline: boolean = false;
     @Input() title: string = "لورم ایپسوم";
-    @Input() date: string = "23 اردیبهشت 1423";
+    @Input() category: string;
     @Input() visit: number = 0;
     @Input() like:  number = 0;
+    @Input() imageURL: string;
+
+    constructor() {}
 
     ngOnInit() {
     }
