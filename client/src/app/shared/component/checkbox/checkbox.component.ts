8448b95c50e3a6f8eed23239322f45cf67ea9fdd
RezaNrzdh <reza.nrzdh@gmail.com>
2023-11-20 10:54:55 +0330

    edit checkbox component

--- client/src/app/shared/component/checkbox/checkbox.component.ts
+++ client/src/app/shared/component/checkbox/checkbox.component.ts
@@ -1,14 +1,38 @@
 import {Component, Input, OnInit} from "@angular/core";
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
 
 @Component({
     selector: "app-checkbox",
     templateUrl: "checkbox.component.html",
-    styleUrls: ["checkbox.component.scss"]
+    styleUrls: ["checkbox.component.scss"],
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: CheckboxComponent,
+            multi: true
+        }
+    ]
 })
-export class CheckboxComponent implements OnInit {
+export class CheckboxComponent implements OnInit, ControlValueAccessor {
 
     constructor() {}
 
+    value: string = "";
+    onChange: (value: any) => void;
+    onTouched: () => void;
+
     ngOnInit(): void {
+    }
+
+    writeValue(obj: any) {
+        this.value = obj;
+    }
+
+    registerOnChange(fn: any) {
+        this.onChange = fn;
+    }
+
+    registerOnTouched(fn: any) {
+        this.onTouched = fn;
     }
 }
