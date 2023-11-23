c755dd215fe0336dfffaeb3c9d036b357b0aaab1
reza.nrzdh@gmail.com <reza.nrzdh@gmail.com>
2023-11-24 00:48:38 +0330

    update experience component

--- client/src/app/core/services/skills.service.ts
+++ client/src/app/core/services/skills.service.ts
@@ -6,28 +6,28 @@ export class SkillsService {
     data: any = [
         {
             title: "طراح وب و موبایل",
-            img: "assets/images/skill/uiux.svg",
+            img: "uiux",
             desc: "به نرم افزارهای طراحی وب مانند Figma و Adobe XD و Balsamiq کاملا مسلط هستم و با مجموعه های زیادی در این حوزه همکاری می‌کنم.",
             levelName: "Senior",
             level: 3
         },
         {
             title: "توسعه دهنده وب",
-            img: "assets/images/skill/webdev.svg",
+            img: "webdev",
             desc: "توانایی کار با زبان های برنامه نویسی مانند JavaScript و TypeScript و فریمورک هایی مانند Angular و NextJs و همچنین پلتفرم NodeJs رو دارم.",
             levelName: "Mid Level",
             level: 2
         },
         {
             title: "طراح گرافیست",
-            img: "assets/images/skill/graphic.svg",
+            img: "graphic",
             desc: "به نرم افزارهای طراحی گرافیک 2 و 3 بعدی مانند Blender و Photoshop و Illustrator کاملا مسلط هستم و با مجموعه های زیادی در این حوزه همکاری می‌کنم.",
             levelName: "Expert",
             level: 4
         },
         {
             title: "توسعه دهنده بازی",
-            img: "assets/images/skill/gamedev.svg",
+            img: "gamedev",
             desc: "چون در این حوزه می‌تونم از توانایی گرافیکی و کد نویسیم بهره ببرم، با موتور بازیسازی Unreal Engine و زبان برنامه نویسی  ++C مشغول به توسعه بازی هستم.",
             levelName: "Junior",
             level: 1
