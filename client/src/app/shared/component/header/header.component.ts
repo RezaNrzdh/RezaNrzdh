44b4b0e2e3027ea53071c422b0ddfe791d06523f
RezaNrzdh <reza.nrzdh@gmail.com>
2023-12-07 02:25:25 +0330

    responsive updates

--- client/src/app/shared/component/header/header.component.ts
+++ client/src/app/shared/component/header/header.component.ts
@@ -1,16 +1,39 @@
-import {Component, HostBinding} from "@angular/core";
+import {Component, DoCheck, HostBinding} from "@angular/core";
 import {NavigationEnd, Router} from "@angular/router";
+import {ResponsiveService} from "../../../core/services/responsive.service";
 
 @Component({
     selector: "app-header",
     templateUrl: "header.component.html",
     styleUrls: ["header.component.scss"]
 })
-export class HeaderComponent {
+export class HeaderComponent implements DoCheck {
 
     internalPages: boolean = false;
+    ShowSidebar: boolean = false;
 
-    constructor(private router: Router ) {
+    isSmall: boolean = false;
+    isMedium: boolean = false;
+    isLarge: boolean = false;
+
+    constructor(private router: Router, private responsiveService: ResponsiveService) {
+
+        this.IsInternalPage(this.router);
+    }
+
+    @HostBinding('class.internalPages') get t() { return this.internalPages };
+
+    ngDoCheck() {
+        this.isSmall = this.responsiveService.getSmall;
+        this.isMedium = this.responsiveService.getMedium;
+        this.isLarge = this.responsiveService.getLarge;
+    }
+
+    OnToggleSidebar(): void {
+        this.ShowSidebar = !this.ShowSidebar;
+    }
+
+    IsInternalPage(router: Router): void {
         this.router.events.subscribe((event) => {
             if(event instanceof NavigationEnd){
                 if(router.url === "/"){
@@ -22,6 +45,5 @@ export class HeaderComponent {
             }
         });
     }
-
-    @HostBinding('class.internalPages') get t() { return this.internalPages };
 }
+
