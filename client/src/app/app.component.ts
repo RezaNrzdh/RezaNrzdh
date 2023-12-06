2c811b925b97bd4bcb19c9052cc95cf65475c021
RezaNrzdh <reza.nrzdh@gmail.com>
2023-12-07 02:24:51 +0330

    responsive service

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import {Component, enableProdMode} from '@angular/core';
+import {BreakpointObserver} from "@angular/cdk/layout";
+import {ResponsiveEnum} from "./core/enum/responsive.enum";
+import {ResponsiveService} from "./core/services/responsive.service";
+
 enableProdMode();
 
 @Component({
@@ -9,5 +13,12 @@ enableProdMode();
 export class AppComponent {
     title = 'RezaNrzdh';
 
-    constructor() {}
+    constructor(private reponsiveService: ResponsiveService ,private breakpointObserver: BreakpointObserver) {
+        let responsiveArray = [ResponsiveEnum.SMALL, ResponsiveEnum.MEDIUM, ResponsiveEnum.LARGE];
+        this.breakpointObserver.observe(responsiveArray).subscribe((value) => {
+            this.reponsiveService.setSmall  = value.breakpoints[ResponsiveEnum.SMALL];
+            this.reponsiveService.setMedium = value.breakpoints[ResponsiveEnum.MEDIUM];
+            this.reponsiveService.setLarge  = value.breakpoints[ResponsiveEnum.LARGE];
+        })
+    }
 }
