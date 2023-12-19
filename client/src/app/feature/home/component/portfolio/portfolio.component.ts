4b5004e443f376d38d5c8645700808c3d01cc74b
RezaNrzdh <reza.nrzdh@gmail.com>
2023-12-19 10:59:41 +0330

    update portfolio component

--- client/src/app/feature/home/component/portfolio/portfolio.component.ts
+++ client/src/app/feature/home/component/portfolio/portfolio.component.ts
@@ -1,19 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, DoCheck, OnInit} from '@angular/core';
 import {PortfolioService} from "../../../../core/services/portfolio.service";
+import {ResponsiveService} from "../../../../core/services/responsive.service";
 
 @Component({
     selector: 'app-home-portfolio',
     templateUrl: './portfolio.component.html',
     styleUrls: ['./portfolio.component.scss']
 })
-export class PortfolioComponent implements OnInit {
+export class PortfolioComponent implements OnInit, DoCheck {
 
     data: any;
+    isSmall: boolean = false;
+    options: Array<string> = ['طراحی وب','توسعه وب','بازیسازی','برندینگ','گرافیک'];
 
-    constructor(private portfolioService: PortfolioService ) { }
+    constructor(private portfolioService: PortfolioService, private responsiveService: ResponsiveService ) { }
 
     ngOnInit(): void {
         this.data = this.portfolioService.GetTopPortfolio();
+    }
+
+    ngDoCheck() {
+        this.isSmall = this.responsiveService.getSmall;
     }
 
 }
