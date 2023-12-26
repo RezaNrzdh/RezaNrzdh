c45a649862518e590b1ed0f15dfdcdb002864863
RezaNrzdh <reza.nrzdh@gmail.com>
2023-12-26 15:04:05 +0330

    responsive Order page

--- client/src/app/feature/order/order.component.ts
+++ client/src/app/feature/order/order.component.ts
@@ -1,21 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, DoCheck, OnInit} from '@angular/core';
 import {OrdersService} from "../../core/services/orders.service";
+import {ResponsiveService} from "../../core/services/responsive.service";
+import {createObject} from "rxjs/internal/util/createObject";
 
 @Component({
     selector: 'app-order',
     templateUrl: './order.component.html',
     styleUrls: ['./order.component.scss']
 })
-export class OrderComponent implements OnInit {
+export class OrderComponent implements OnInit, DoCheck {
 
     data: any;
+    isSmall: boolean = false;
     selectedOrder: number = 0;
     selectedSubOrder: number = 0;
 
-    constructor(private orderService: OrdersService) {}
+    constructor(private orderService: OrdersService, private responsiveService: ResponsiveService) {}
 
     ngOnInit(): void {
         this.data = this.orderService.getOrders();
+    }
+
+    ngDoCheck() {
+        this.isSmall = this.responsiveService.getSmall;
     }
 
     SetSelectedOrder(value: number): void{
@@ -26,5 +33,4 @@ export class OrderComponent implements OnInit {
     SetSelectedSubOrder(value: number): void{
         this.selectedSubOrder = value;
     }
-
 }
