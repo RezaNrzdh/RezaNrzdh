32b9fe9ce637264acb7cdf5a26063dc66c2bbd0f
RezaNrzdh <reza.nrzdh@gmail.com>
2023-12-27 00:30:00 +0330

    responsive about page

--- client/src/app/feature/about/about.component.ts
+++ client/src/app/feature/about/about.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, DoCheck, OnInit} from '@angular/core';
 import {AboutService} from "../../core/services/about.service";
+import {ResponsiveService} from "../../core/services/responsive.service";
 
 @Component({
     selector: 'app-about',
     templateUrl: './about.component.html',
     styleUrls: ['./about.component.scss']
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent implements OnInit, DoCheck {
 
     data: any;
     tab: number = 0;
+    isSmall: boolean = false;
+    options: Array<string> = ['طراحی UIUX','برنامه نویسی','گرافیک دو بعدی','گرافیک سه بعدی','بازیسازی'];
 
-    constructor(private AboutService: AboutService) { }
+    constructor(private AboutService: AboutService, private responsiveService: ResponsiveService) { }
 
     ngOnInit(): void {
         this.data = this.AboutService.GetAbout();
@@ -20,6 +23,10 @@ export class AboutComponent implements OnInit {
 
     SetTabIndex(value: number): void {
         this.tab = value;
+    }
+
+    ngDoCheck() {
+        this.isSmall = this.responsiveService.getSmall;
     }
 
 }
