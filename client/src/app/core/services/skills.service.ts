320b7ce9edd0d599352f0e05e2dfe228dd40c823
RezaNrzdh <reza.nrzdh@gmail.com>
2023-12-27 13:49:02 +0330

    improve skills component

--- client/src/app/core/services/skills.service.ts
+++ client/src/app/core/services/skills.service.ts
@@ -1,43 +1,15 @@
 import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {environment} from "../../../environments/environment";
 
 @Injectable()
 export class SkillsService {
 
-    data: any = [
-        {
-            title: "طراح وب و موبایل",
-            img: "uiux",
-            desc: "به نرم افزارهای طراحی وب مانند Figma و Adobe XD و Balsamiq کاملا مسلط هستم و با مجموعه های زیادی در این حوزه همکاری می‌کنم.",
-            levelName: "Senior",
-            level: 3
-        },
-        {
-            title: "توسعه دهنده وب",
-            img: "webdev",
-            desc: "توانایی کار با زبان های برنامه نویسی مانند JavaScript و TypeScript و فریمورک هایی مانند Angular و NextJs و همچنین پلتفرم NodeJs رو دارم.",
-            levelName: "Mid Level",
-            level: 2
-        },
-        {
-            title: "طراح گرافیست",
-            img: "graphic",
-            desc: "به نرم افزارهای طراحی گرافیک 2 و 3 بعدی مانند Blender و Photoshop و Illustrator کاملا مسلط هستم و با مجموعه های زیادی در این حوزه همکاری می‌کنم.",
-            levelName: "Expert",
-            level: 4
-        },
-        {
-            title: "توسعه دهنده بازی",
-            img: "gamedev",
-            desc: "چون در این حوزه می‌تونم از توانایی گرافیکی و کد نویسیم بهره ببرم، با موتور بازیسازی Unreal Engine و زبان برنامه نویسی  ++C مشغول به توسعه بازی هستم.",
-            levelName: "Junior",
-            level: 1
-        }
-    ]
-
-    constructor() {
+    constructor(private http: HttpClient) {
     }
 
-    getSkills = (): any => {
-        return this.data;
+    getSkills = (): Observable<any> => {
+        return this.http.get(`${environment.server}/skills`);
     }
 }
