d3c5868975744bd833f6f4425965a67a6e2920ff
RezaNrzdh <reza.nrzdh@gmail.com>
2024-01-05 00:34:11 +0330

    بهبود دکمه ورود

--- client/src/app/feature/auth/login/login.component.ts
+++ client/src/app/feature/auth/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {AuthService} from "../../../core/services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'app-login',
@@ -11,7 +12,7 @@ export class LoginComponent implements OnInit {
 
     loginForm: FormGroup | any;
 
-    constructor(private authService: AuthService) { }
+    constructor(private authService: AuthService, private router: Router) { }
 
     ngOnInit(): void {
         this.loginForm = new FormGroup({
@@ -21,14 +22,17 @@ export class LoginComponent implements OnInit {
     }
 
     onSubmit = () => {
+        if(this.loginForm.status === "INVALID") return;
+
         this.authService.SignIn(this.loginForm.value).subscribe({
             next: ((value: any) => {
-                console.log(value);
+                this.router.navigate(["/"]);
             }),
             error: ((error: any) => {
                 console.log(error)
             })
-        })
+        });
+
     }
 
 }
