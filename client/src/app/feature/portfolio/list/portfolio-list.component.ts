60de1dce44a87bb01396dd0613f324501e3dda96
RezaNrzdh <reza.nrzdh@gmail.com>
2024-02-03 21:16:36 +0330

    بهبود کامپوننت portfolio-list

--- client/src/app/feature/portfolio/list/portfolio-list.component.ts
+++ client/src/app/feature/portfolio/list/portfolio-list.component.ts
@@ -4,6 +4,9 @@ import {ResponsiveService} from "../../../core/services/responsive.service";
 import {PortfolioModel} from "../../../core/models/portfolio.model";
 import {Subscription} from "rxjs";
 import {ResponsiveEnum} from "../../../core/enum/responsive.enum";
+import {SortEnum} from "../../../core/enum/sort.enum";
+import {CategoryConstant} from "../../../core/constant/category.constant";
+import {SortConstant} from "../../../core/constant/sort.constant";
 
 @Component({
     selector: 'app-portfolio-list',
@@ -15,8 +18,12 @@ export class PortfolioListComponent implements OnInit, OnDestroy {
     data: Array<PortfolioModel>;
     isXSmall: boolean = false;
     isSmall: boolean = false;
-    options: Array<string> = ['همه','طراحی وب','توسعه وب','بازیسازی','گرافیک'];
-    sorts: Array<string> = ['جدیدترین','پربازدیدترین'];
+    sortNum: SortEnum = SortEnum.NEWEST;
+    options: Array<string> = ['همه', ...CategoryConstant];
+    sorts: Array<string> = SortConstant;
+    lt: number = 0;
+    pagination: number = 8;
+    tab: number = 0;
     sub: Subscription;
 
     constructor(private portfolioService: PortfolioService, private responsiveService: ResponsiveService) {
@@ -29,18 +36,45 @@ export class PortfolioListComponent implements OnInit, OnDestroy {
     }
 
     ngOnInit(): void {
-        this.portfolioService.GetAllPortfolio().subscribe({
+        this.OnGetAllPortfolio();
+    }
+
+    OnGetPortfolioByCategory(value: number): void {
+        this.lt = 0;
+        this.portfolioService.GetPortfolioByCategory(value).subscribe({
             next: ((value: any) => {
                 this.data = value;
-            }),
-            error: ((error: any) => {
-                console.log(error)
+            })
+        })
+    }
+
+    OnGetAllPortfolio(): void {
+        this.portfolioService.GetAllPortfolio(this.lt, this.pagination).subscribe({
+            next: ((value: any) => {
+                this.data = value;
+                this.lt = value[this.data.length - 1]._id;
+            })
+        });
+    }
+
+    OnGetMorePortofilo(): void {
+        this.portfolioService.GetAllPortfolio(this.lt, this.pagination).subscribe({
+            next: ((value: any) => {
+                this.data = this.data.concat(value);
+                this.lt = value[value.length - 1]._id;
             })
         });
     }
 
     ngOnDestroy() {
         this.sub.unsubscribe();
+    }
+
+    SetSort(value: number): void {
+        switch (value){
+            case 1: this.sortNum = SortEnum.NEWEST; break;
+            case 2: this.sortNum = SortEnum.MOSTVISITED; break;
+        }
     }
 
 }
