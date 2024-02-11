6f00ffbe2236f54511aeceea8356600a637ae794
RezaNrzdh <reza.nrzdh@gmail.com>
2024-02-11 22:02:52 +0330

    بهبود کامپوننت دکمه

--- client/src/app/shared/component/button/button.component.ts
+++ client/src/app/shared/component/button/button.component.ts
@@ -7,9 +7,9 @@ import {Component, EventEmitter, Input, OnInit, Output} from "@angular/core";
 })
 export class ButtonComponent implements OnInit {
 
-    @Input() mode: "filled" | "outlined" | "ghost" | "submit";
+    @Input() mode: "filled" | "outlined" | "ghost" | "submit" = 'filled';
     @Input() action: "submit" | "button" | "reset";
-    @Input() color: "primary" | "secondary" | "dark" | "white" | "dark2";
+    @Input() color: "primary" | "secondary" | "dark" | "white" | "dark2" = 'primary';
     @Input() isLink: true | false = false;
     @Input() icon?: string;
     @Input() href?: string;
