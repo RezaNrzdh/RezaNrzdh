615f1d5e1d9506a2d19ebb93788b7fa7fe43ac88
RezaNrzdh <reza.nrzdh@gmail.com>
2024-03-01 01:14:36 +0330

    improve dropdown component

--- client/src/app/shared/component/dropdown/dropdown.component.ts
+++ client/src/app/shared/component/dropdown/dropdown.component.ts
@@ -16,6 +16,7 @@ import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
 export class DropdownComponent implements OnInit, ControlValueAccessor {
 
     @Input() options: Array<string>;
+    @Input() numValue: boolean = true;
     @Input() darkmode: boolean = false;
 
     value: string = "";
