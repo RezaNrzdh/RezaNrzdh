fb73af236a76974182a5fb2b651997a18419c002
RezaNrzdh <reza.nrzdh@gmail.com>
2024-04-03 19:02:15 +0330

    improve newsletter

--- client/src/app/shared/component/newsletter/newsletter.component.ts
+++ client/src/app/shared/component/newsletter/newsletter.component.ts
@@ -1,30 +1,36 @@
 import {Component, OnInit} from "@angular/core";
 import {IconComponent} from "../icon/icon.component";
 import {NewsletterService} from "../../../core/services/newsletter.service";
-import {FormsModule} from "@angular/forms";
+import {FormControl, FormGroup, FormsModule, Validators, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
     selector: "app-newsletter",
     templateUrl: "newsletter.component.html",
     styleUrls: ["newsletter.component.scss"],
+    standalone: true,
     imports: [
         IconComponent,
-        FormsModule
-    ],
-    standalone: true
+        FormsModule,
+        ReactiveFormsModule
+    ]
 })
 export class NewsletterComponent implements OnInit {
 
     newsletterInput: string = "";
+    newsletterForm: FormGroup | any;
 
-    constructor(private newsletterService: NewsletterService) {}
+    constructor(private newsletterService: NewsletterService) {
+        this.newsletterForm = new FormGroup({
+            "email": new FormControl(null, [Validators.required, Validators.email])
+        });
+    }
 
     ngOnInit(): void {
     }
 
     SubmitNewsletter(): void {
-        if(this.newsletterInput == "") return;
-        this.newsletterService.CreateNewsletter({email: this.newsletterInput}).subscribe({
+        if(this.newsletterForm.status === "INVALID") return;
+        this.newsletterService.CreateNewsletter(this.newsletterForm.value).subscribe({
             next: ((value) => {
                 this.newsletterInput = "";
             })
