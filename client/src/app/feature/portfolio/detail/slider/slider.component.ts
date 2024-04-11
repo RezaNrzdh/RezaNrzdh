1d4a531cb02372a8c30bbb09344b7edc1b0f7be3
reza.nrzdh@gmail.com <reza.nrzdh@gmail.com>
2024-04-11 16:55:15 +0330

    update slider

--- client/src/app/feature/portfolio/detail/slider/slider.component.ts
+++ client/src/app/feature/portfolio/detail/slider/slider.component.ts
@@ -19,18 +19,17 @@ import {ImagePathPipe} from "../../../../shared/pipe/image-path.pipe";
 export class SliderComponent implements OnInit {
 
     @Input() image: Array<any> = [];
-    @ViewChild("sliderWrapper") sliderWrapper: ElementRef;
 
     currentImageSize: number = 0;
     currentImage: number = 0;
     transformX: number = 0;
 
-    sliderClass: boolean = false;
+    transitionClass: boolean = false;
     start: number = 0;
+    grabbing: boolean = false;
     offset: number = 0;
 
-    constructor(private renderer: Renderer2) {
-    }
+    constructor() {}
 
     ngOnInit() {
         this.transformX = 0;
@@ -44,44 +43,27 @@ export class SliderComponent implements OnInit {
     ShowNextImage(): void {
         if(this.currentImage >= this.image.length - 1) return;
 
-        this.sliderClass = true;
+        this.transitionClass = true;
         this.currentImage++;
-        this.transformX += this.currentImageSize;
+        this.transformX += 100;
     }
 
     ShowPrevImage(): void {
         if(this.currentImage <= 0) return;
 
-        this.sliderClass = true;
+        this.transitionClass = true;
         this.currentImage--;
-        this.transformX -= this.currentImageSize;
+        this.transformX -= 100;
     }
 
     Start(e: any): void {
-        console.log(e.clientX);
+        this.grabbing = true;
         this.start = e.offsetX;
     }
 
-    Over(e: any): void {
-        this.sliderClass = false;
-        this.offset = e.offsetX - this.start;
-
-        if(this.transformX < 0) {
-            this.transformX = 0;
-        }
-
-        if(e.clientX - this.start < (this.currentImageSize / 2)){
-            this.transformX += this.offset;
-        }
-        else {
-        }
-
-        if(this.transformX > this.currentImageSize * 2) {
-            this.transformX = this.currentImageSize * 2;
-        }
-    }
-
     End(e: any): void {
-        console.log(e.clientX);
+        this.grabbing = false;
+        if(this.start < e.clientX) this.ShowNextImage();
+        else this.ShowPrevImage();
     }
 }
